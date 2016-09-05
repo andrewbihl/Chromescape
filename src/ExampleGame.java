897283ef2060469bfedafcb292848d78966ae590
andrewbihl <andrewbihlva@gmail.com>
2016-09-05 19:57:31 -0400

    Set up basic physics of ship flying

--- src/ExampleGame.java
+++ src/ExampleGame.java
@@ -5,29 +5,37 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import java.util.*;
 
 
 /**
  * Separate the game code from some of the boilerplate code.
  * 
- * @author Robert C. Duvall
+ * @author Andrew Bihl
  */
 class ExampleGame {
     public static final String TITLE = "My Game";
     public static final int KEY_INPUT_SPEED = 5;
     private static final double GROWTH_RATE = 1.1;
-    private static final int BOUNCER_SPEED = 30;
-
+    private static int NUMBER_OF_OBSTACLES_PER_LINE = 8;
+    private static final double ACCELERATION_RATE = 1.2;
+    private static final double DECELERATION_RATE = 1.8;
+    private static final double SHIP_WIDTH = 25;
+    private static final double SHIP_HEIGHT = 40;
+    private static final double MAX_SPEED = 4.0;
+    
     private Scene myScene;
-    private ImageView myBouncer;
+    private Polygon myShip;
     private Rectangle myTopBlock;
     private Rectangle myBottomBlock;
-    private Button myButton;
+    private ArrayList<Obstacle> obstacles;
 
-    private Boolean goRight = true;
-
+    private int blockWidth;
+    private double shipSpeed = 0.0;
+    
     /**
      * Returns name of the game.
      */
@@ -42,24 +50,22 @@ class ExampleGame {
         // create a scene graph to organize the scene
         Group root = new Group();
         // create a place to see the shapes
-        myScene = new Scene(root, width, height, Color.WHITE);
+        myScene = new Scene(root, width, height, Color.BLACK);
+        blockWidth = (int) (width * (1.0/30));
         // make some shapes and set their properties
-        Image image = new Image(getClass().getClassLoader().getResourceAsStream("duke.gif"));
-        myBouncer = new ImageView(image);
-        myButton = new Button("Change Direction!");
-        myButton.setOnAction(e -> handleDirectionButtonTap());
+//        Image image = new Image(getClass().getClassLoader().getResourceAsStream("duke.gif"));
+        makeShip();
         // x and y represent the top left corner, so center it
-        myBouncer.setX(width / 2 - myBouncer.getBoundsInLocal().getWidth() / 2);
-        myBouncer.setY(height / 2  - myBouncer.getBoundsInLocal().getHeight() / 2);
+//        myShip.setX(width / 2 - myShip.getBoundsInLocal().getWidth() / 2);
+//        myShip.setY(height / 2  - myShip.getBoundsInLocal().getHeight() / 2);
         myTopBlock = new Rectangle(width / 2 - 25, height / 2 - 100, 50, 50);
         myTopBlock.setFill(Color.RED);
         myBottomBlock = new Rectangle(width / 2 - 25, height / 2 + 50, 50, 50);
         myBottomBlock.setFill(Color.BISQUE);
         // order added to the group is the order in which they are drawn
-        root.getChildren().add(myBouncer);
+        root.getChildren().add(myShip);
         root.getChildren().add(myTopBlock);
         root.getChildren().add(myBottomBlock);
-        root.getChildren().add(myButton);
         // respond to input
         myScene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
         myScene.setOnMouseClicked(e -> handleMouseInput(e.getX(), e.getY()));
@@ -72,19 +78,43 @@ class ExampleGame {
      * Note, there are more sophisticated ways to animate shapes,
      * but these simple ways work too.
      */
+    private void makeShip(){
+    	double mid = SHIP_WIDTH / 2.0;
+    	double height = myScene.getHeight();
+    	double buffer = 25;
+    	Polygon ship = new Polygon();
+    	ship.getPoints().addAll(new Double[]{
+    			mid, height - buffer - 7,
+    			0.0, height - buffer,
+    			mid,  height - (buffer + SHIP_HEIGHT),
+    			mid * 2.0, height - buffer}
+    			);
+    	ship.setFill(Color.WHITE);
+    	ship.setTranslateX(myScene.getWidth()/2.0);
+    	myShip = ship;
+    }
+    
     public void step (double elapsedTime) {
+    	//increment each block
+    	
+    	
+    	
+//    	System.out.println(myShip.translateXProperty().doubleValue());
         // update attributes
-    	if (myBouncer.getX() + myBouncer.getLayoutBounds().getWidth() >= myScene.getWidth()){
-    		goRight = false;
-    	} else if (myBouncer.getX()<=0){
-    		goRight = true;
-    	}
-    	if (goRight){
-    		myBouncer.setX(myBouncer.getX() + BOUNCER_SPEED * elapsedTime);
+    	double currentX = myShip.getTranslateX();
+//    	System.out.println(currentX);
+    	if (currentX >= myScene.getWidth()) {
+//    		myShip.setX(myScene.getWidth() - myShip.getLayoutBounds().getWidth() - 0.01);
+    		myShip.setTranslateX(0.01);
+    	} 
+    	else if (currentX < 0 - SHIP_WIDTH){
+    		myShip.setTranslateX(myScene.getWidth() - 0.01);
+//    		myShip.setX(0.01);
     	}
     	else{
-    		myBouncer.setX(myBouncer.getX() - BOUNCER_SPEED * elapsedTime);
+    		myShip.setTranslateX(currentX + shipSpeed);
     	}
+//		shipSpeed *= 0.8;
         myTopBlock.setRotate(myBottomBlock.getRotate() - 1);
         myBottomBlock.setRotate(myBottomBlock.getRotate() + 1);
         
@@ -98,39 +128,60 @@ class ExampleGame {
             myTopBlock.setFill(Color.RED);
         }
         // with images can only check bounding box
-        if (myBottomBlock.getBoundsInParent().intersects(myBouncer.getBoundsInParent())) {
+        if (myBottomBlock.getBoundsInParent().intersects(myShip.getBoundsInParent())) {
             myBottomBlock.setFill(Color.BURLYWOOD);
         }
         else {
             myBottomBlock.setFill(Color.BISQUE);
         }
     }
-
-
-    private void handleDirectionButtonTap(){
-    	goRight = !goRight;
+    
+    private void generateLineOfObstacles() {
+    	for (int i = 0; i < NUMBER_OF_OBSTACLES_PER_LINE; i ++){
+    		Obstacle obstacle = generateObstacle();
+    		obstacle.setX(10*i);
+    	}
+    }
+    
+    private Obstacle generateObstacle(){
+    	Obstacle obstacle = new Obstacle();
+    	obstacle.setFill(Color.WHITE);
+    	return obstacle;
     }
     
     // What to do each time a key is pressed
     private void handleKeyInput (KeyCode code) {
         switch (code) {
             case RIGHT:
-                myTopBlock.setX(myTopBlock.getX() + KEY_INPUT_SPEED);
+                accelerateShip(true);
                 break;
             case LEFT:
-                myTopBlock.setX(myTopBlock.getX() - KEY_INPUT_SPEED);
-                break;
-            case UP:
-                myTopBlock.setY(myTopBlock.getY() - KEY_INPUT_SPEED);
-                break;
-            case DOWN:
-                myTopBlock.setY(myTopBlock.getY() + KEY_INPUT_SPEED);
+                accelerateShip(false);
                 break;
             default:
                 // do nothing
         }
     }
 
+    private void accelerateShip(Boolean right){
+    	System.out.println(shipSpeed);
+    	if (right){
+        	shipSpeed += 0.3;
+    	}
+    	else{
+        	shipSpeed -= 0.3;
+    	}
+    	double overflow = Math.abs(shipSpeed) - MAX_SPEED;
+    	if (overflow > 0){
+    		if (shipSpeed > 0){
+    			shipSpeed -= overflow;
+    		} 
+    		else {
+    			shipSpeed += overflow;
+    		}
+    	}
+    }
+    
     // What to do each time a key is pressed
     private void handleMouseInput (double x, double y) {
         if (myBottomBlock.contains(x, y)) {
--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,10 +1,16 @@
+import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 
 public class Obstacle extends Rectangle {
 	public CharacterColor color;
 	
+	public Obstacle(){
+		super();
+	}
+	
 	public Obstacle(CharacterColor color){
 		super();
-		this.color= color;
+		this.color = color;
+		this.setFill(Color.RED);
 	}
 }
